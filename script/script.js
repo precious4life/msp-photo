daf3488eb269194b8f988d9cb445500316d74c2a
precious4life <preciousorugiengha@gmail.com>
2023-12-06 19:18:27 +0100

    Errrors corrected

--- script/script.js
+++ script/script.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const checkbox = document.getElementById('check');
+    const burgerIcon = document.getElementById('burger-icon');
     const navUL = document.querySelector('nav ul');
 
-    checkbox.addEventListener('change', function() {
-        navUL.classList.toggle('show', checkbox.checked);
+    burgerIcon.addEventListener('click', function() {
+        navUL.classList.toggle('show');
     });
 });
